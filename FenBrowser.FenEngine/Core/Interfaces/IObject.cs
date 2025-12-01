dc9b39fb1bd6ea2b809a6129c40f92a33f86a7fd
udaie-a <udayfluer@gmail.com>
2025-12-02 00:11:11 +0530

    core: contracts for Implement JavaScript loops (while, for) and operators (new, comma) plus null/undefined literals

---
+++ FenBrowser.FenEngine/Core/Interfaces/IObject.cs
@@ -0,0 +1,46 @@
+using System.Collections.Generic;
+
+namespace FenBrowser.FenEngine.Core.Interfaces
+{
+    /// <summary>
+    /// Represents a JavaScript object.
+    /// Extensible for different object implementations (Array, Date, RegExp, etc.)
+    /// </summary>
+    public interface IObject
+    {
+        /// <summary>
+        /// Get a property value
+        /// </summary>
+        IValue Get(string key);
+
+        /// <summary>
+        /// Set a property value
+        /// </summary>
+        void Set(string key, IValue value);
+
+        /// <summary>
+        /// Check if object has a property
+        /// </summary>
+        bool Has(string key);
+
+        /// <summary>
+        /// Delete a property
+        /// </summary>
+        bool Delete(string key);
+
+        /// <summary>
+        /// Get all enumerable property keys
+        /// </summary>
+        IEnumerable<string> Keys();
+
+        /// <summary>
+        /// Get the prototype object (for inheritance)
+        /// </summary>
+        IObject GetPrototype();
+
+        /// <summary>
+        /// Set the prototype object
+        /// </summary>
+        void SetPrototype(IObject prototype);
+    }
+}
