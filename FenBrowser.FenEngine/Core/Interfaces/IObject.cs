3d96708ac84de8dfceda6d083883742b3d7961f1
udaie-a <udayfluer@gmail.com>
2025-12-22 22:17:55 +0530

    core: contracts for Phase E & F: Storage (Partitioned) and ServiceWorker (Read-Only Interception)

--- FenBrowser.FenEngine/Core/Interfaces/IObject.cs
+++ FenBrowser.FenEngine/Core/Interfaces/IObject.cs
@@ -11,27 +11,27 @@ namespace FenBrowser.FenEngine.Core.Interfaces
         /// <summary>
         /// Get a property value
         /// </summary>
-        IValue Get(string key);
+        IValue Get(string key, IExecutionContext context = null);
 
         /// <summary>
         /// Set a property value
         /// </summary>
-        void Set(string key, IValue value);
+        void Set(string key, IValue value, IExecutionContext context = null);
 
         /// <summary>
         /// Check if object has a property
         /// </summary>
-        bool Has(string key);
+        bool Has(string key, IExecutionContext context = null);
 
         /// <summary>
         /// Delete a property
         /// </summary>
-        bool Delete(string key);
+        bool Delete(string key, IExecutionContext context = null);
 
         /// <summary>
         /// Get all enumerable property keys
         /// </summary>
-        IEnumerable<string> Keys();
+        IEnumerable<string> Keys(IExecutionContext context = null);
 
         /// <summary>
         /// Get the prototype object (for inheritance)
