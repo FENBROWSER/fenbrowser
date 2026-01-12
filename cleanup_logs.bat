abbc927d08ea772e87c94a411b3afca90e72c325
udaie-a <udayfluer@gmail.com>
2026-01-12 11:00:08 +0530

    chore: mechanical preparations for feat(rendering): update rendering pipeline, css parsers, and typography

--- cleanup_logs.bat
+++ cleanup_logs.bat
@@ -2,10 +2,9 @@
 set TARGET_DIR=C:\Users\udayk\Videos\FENBROWSER
 
 echo Deleting .log files...
-del /q "%TARGET_DIR%\logs\*.log"
-del /q "%TARGET_DIR%\logs\*.html"
+del /q "%TARGET_DIR%\logs\*"
 del /q "%TARGET_DIR%\*.log"
-del /q "%TARGET_DIR%\debug_screenshot.png"
+del /q "%TARGET_DIR%\*.png"
 
 echo Deleting .txt files...
 del /q "%TARGET_DIR%\*.txt"
