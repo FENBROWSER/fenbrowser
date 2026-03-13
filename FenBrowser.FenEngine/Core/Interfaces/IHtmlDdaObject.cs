cd06e912fb04de92809a47618e00c8d5d876bd49
udaie-a <udayfluer@gmail.com>
2026-03-13 10:34:58 +0530

    core: contracts for feat(runtime): extend host interop and builtin compliance

---
+++ FenBrowser.FenEngine/Core/Interfaces/IHtmlDdaObject.cs
@@ -0,0 +1,9 @@
+namespace FenBrowser.FenEngine.Core.Interfaces
+{
+    /// <summary>
+    /// Marker for host objects that emulate the Annex B [[IsHTMLDDA]] internal slot.
+    /// </summary>
+    public interface IHtmlDdaObject
+    {
+    }
+}
